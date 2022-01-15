44a4cd0f8f8c1b5332a21b8563559581f72f9fe5
Reza Aditya Sutrino <adityareza46@gmail.com>
2022-01-15 10:06:49 +0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Reza Aditya Sutrisno on 15/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
